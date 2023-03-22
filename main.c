83741a895a0952b5a81360f3060443937b49dec8
sickboydie <letov.gelb@gmail.com>
2023-03-22 14:16:33 +0700

    add generation colission

--- main.c
+++ main.c
@@ -3,13 +3,12 @@
 #include <time.h>
 #include <stdbool.h>
 
-int py, px;                                                               // character position
-int ey, ex;                                                                // entity position
-int player_credits = 0;                                                    // player gold-credits-money
-                                            // room variables
+int py, px;                                                              // character position
+int ey, ex;                                                              // entity position
+int player_credits = 0;                                                  // player gold-credits-money
 
 
-bool entity_placed = 0;
+bool entity_placed = 0; 
 bool player_placed = 0;
 int  room_placed   = 0;
 
@@ -17,23 +16,23 @@ int  room_placed   = 0;
 
 int dungeon(int rows, int cols,  char (*map)[cols],int player){             // generate  dungeon map
 
-    bool collision = 0;
+    bool collision;
 
     
     srand(time(NULL));
 
     
 
-    if(!room_placed)
-    {                                                                       // random generate room
+    if(!room_placed)    // random generate room
+    {                                                                       
         int ry,rx; 
         int r_size_y, r_size_x;
         int room_num = rand() % 5 + 10;
-                                            
+        bool collision;                                  
 
 
-        for (int yy = 0; yy <= rows; yy++)                                  // fill dungeon and borders database
-        {   
+        for (int yy = 0; yy <= rows; yy++){                                  // fill dungeon and borders database  
+
             for(int xx = 0; xx <= cols; xx++)
             {
                 // border
@@ -47,21 +46,48 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
             }
         }
 
-        do
-        {
-                    // generate room coords
-            ry = rand() % (rows -3);
-            rx = rand() % (cols -3);
-            // room size
-            r_size_y = rand() % 5 + 4;
-            r_size_x = rand() % 10 + 8;
+        while (room_placed < room_num){   
+            int try_counter =0;// prototyping room until collision
+            do {
+                collision =0;
+                try_counter++;
+                // generate room coords
+                ry = rand() % (rows -3);
+                rx = rand() % (cols -3);
+                // room size
+                r_size_y = rand() % 5 + 4;
+                r_size_x = rand() % 10 + 8;
 
-        // fill DB
-            for (int yy = ry; yy <= ry + r_size_y; yy++)                             // generate room
-            {
-                for(int xx = rx; xx <= rx + r_size_x ; xx++){
+
+                for (int yy = ry; yy <= ry + r_size_y; yy++)                             // generate room
+                {
                     
-                    
+                    for(int xx = rx; xx <= rx + r_size_x ; xx++){
+                        
+                        
+                        if(map[yy][xx] == '%' || map[yy][xx] == ' ' 
+                        || map[yy + 2][xx] == ' ' || map[yy - 2][xx] == ' ' 
+                        || map[yy][xx+2] == ' ' || map[yy][xx-2] == ' ' )
+                        {   
+                            collision = 1;
+                            yy = ry + r_size_y;                                           // exit upper loop
+                            break;                                                        // ... exit from current loop
+                        }
+                    }
+                }
+                if (try_counter > 100){
+                    ry = rx = 3;
+                    r_size_y = r_size_x = 3;
+                    break;
+
+                }
+
+            } while (collision == 1);
+        
+
+
+            for (int yy = ry; yy <= ry + r_size_y; yy++){                             // generate room  // fill DB
+                for(int xx = rx; xx <= rx + r_size_x ; xx++){            
                     if(map[yy][xx] == '%')
                     {   
                         yy = ry + r_size_y;                                           // exit upper loop
@@ -74,7 +100,7 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
                 }
             }
             room_placed++;
-        } while(room_placed < room_num);
+        } 
     }
 
 
@@ -82,21 +108,19 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
 
 
 
-    for (int yy = 0; yy <= rows; yy++)                                       // draw location
-    {
-        for(int xx = 0; xx <= cols; xx++)
-        {
-            if(yy == rows )
-            {
+    for (int yy = 0; yy <= rows; yy++){                                       // draw location
+        for(int xx = 0; xx <= cols; xx++){
+            if(yy == rows ){
+
                 mvaddch(yy,xx, ' ');
             }
-            else if(map[yy][xx] == '%')                                           //  draw border
-            {
+            else if(map[yy][xx] == '%'){                                           //  draw border
+               
                mvaddch(yy,xx, '%');
             }
-            else if (map[yy][xx] == ' ')
-            {
-                 mvaddch(yy,xx, ' ');
+            else if (map[yy][xx] == ' '){
+            
+                mvaddch(yy,xx, ' ');
             }
             else
                mvaddch(yy,xx, '#');
@@ -105,10 +129,9 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
     }
 
 
-    if(!entity_placed)
-    { 
-        do
-        {
+    if(!entity_placed){ 
+        do {
+
             ey = rand() % rows + 1;
             ex = rand() % cols + 1;
         }
@@ -116,10 +139,10 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
         entity_placed = 1;
     }
 
-    if(!player_placed)
-    { 
-        do
-        {
+    if(!player_placed){ 
+
+        do {
+
             py = rand() % rows;
             px = rand() % cols;
         }
@@ -136,8 +159,8 @@ int dungeon(int rows, int cols,  char (*map)[cols],int player){             // g
 
 
 
-    if (py == ey && px == ex)
-    {
+    if (py == ey && px == ex){
+
         entity_placed = 0;
         player_credits += rand() % 10 + 1;
         room_placed = 0;
